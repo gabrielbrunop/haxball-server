0e999874f32aa106275af12b2cd585932c3980c1
gabrielbrop <gabrielbrunoes@gmail.com>
2021-08-16 17:46:41 -0300

    Remove old unused files from dist

--- dist/utils/ConnectInterface.d.ts
+++
@@ -1,8 +0,0 @@
-import { DebuggingClient } from "./DebuggingClient";
-export declare class ConnectInterface {
-    constructor(port: number, wsPort: number, debuggingClient: DebuggingClient);
-    private openExpressServer;
-    private openWSServer;
-    private addRoomToList;
-    private removeRoomFromList;
-}
--- dist/utils/isPortTaken.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function isPortTaken(port: number): Promise<boolean>;
